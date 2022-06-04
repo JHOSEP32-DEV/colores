665dc4b523ba1513479c5466a90454982ada6e3d
JHOSEP32-DEV <jhosep32.dev@gmail.com>
2022-06-04 13:59:33 -0500

    Commit inicial
    Se crea aplicacion en springboot con spring initializr y las respectivas dependencias necesarias para el proyecto

---
+++ colores-api/src/main/java/com/jhosep32dev/coloresapi/ColoresApiApplication.java
@@ -0,0 +1,13 @@
+package com.jhosep32dev.coloresapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColoresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColoresApiApplication.class, args);
+	}
+
+}
---
+++ colores-api/src/test/java/com/jhosep32dev/coloresapi/ColoresApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhosep32dev.coloresapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColoresApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
