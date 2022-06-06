55afcd679f7edc0f62eaf90d1289eb15c70d8cc2
JHOSEP32-DEV <jhosep32.dev@gmail.com>
2022-06-05 19:23:21 -0500

    se gregan rutas para los componentes
    con sus resolvers y guards

--- colores-cli/src/app/app-routing.module.ts
+++ colores-cli/src/app/app-routing.module.ts
@@ -5,6 +5,13 @@ import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 import {GuestGuard} from "./guards/guest.guard";
 import {LoginComponent} from "./pages/user/login/login.component";
 import {NotFoundComponent} from "./pages/errors/not-found/not-found.component";
+import {ColorsComponent} from "./pages/colors/colors.component";
+import {AuthGuard} from "./guards/auth.guard";
+import {ColorsResolver} from "./resolvers/colors.resolver";
+import {ColorResolver} from "./resolvers/color.resolver";
+import {ColorListComponent} from "./pages/colors/color-list/color-list.component";
+import {ColorCreateComponent} from "./pages/colors/color-create/color-create.component";
+import {ColorDetailComponent} from "./pages/colors/color-detail/color-detail.component";
 
 const routes: Routes = [
   {
@@ -20,7 +27,36 @@ const routes: Routes = [
         path: 'home',
         component: DashboardComponent
       },
-
+      {
+        path: 'colors',
+        component: ColorsComponent,
+        canActivate: [AuthGuard],
+        resolve: {
+          colors: ColorsResolver
+        },
+        children: [
+          {
+            path: '',
+            redirectTo: 'list',
+            pathMatch: 'full'
+          },
+          {
+            path: 'list',
+            component: ColorListComponent
+          },
+          {
+            path: 'create',
+            component: ColorCreateComponent
+          },
+          {
+            path: 'detail/:colorId',
+            resolve: {
+              color: ColorResolver
+            },
+            component: ColorDetailComponent
+          }
+        ]
+      }
     ]
   },
   {
